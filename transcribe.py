86c734b9c6f19c06da7ed19c46e37f7f1dadaa58
norbusonam <nsonam1822@gmail.com>
2022-12-26 13:04:38 -0500

    transcribe bug

--- transcribe.py
+++ transcribe.py
@@ -6,7 +6,7 @@ audio_file = sys.argv[1] if len(sys.argv) >= 2 else "./recordings/output.wav"
 
 # transcribe
 model = whisper.load_model("base")
-result = model.transcribe(filepath)
+result = model.transcribe(audio_file)
 
 # print result
 print(result["text"])
